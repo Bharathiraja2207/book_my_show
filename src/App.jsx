a0925d78abb2e4ac81a63eac53780aebbdcb3f71
Bharathiraja2207 <bharathiraja2207@gmail.com>
2023-03-14 14:28:45 +0530

    webcode2

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Movielist } from './Movielist';
+
+export default function App() {
+  
+
+  return (
+    <div className="App">
+    <h2>welcome</h2>
+    <Movielist/>
+    </div>
+  )
+}
+
+
+
