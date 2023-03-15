2bf2b0350024d70f11b016ec6186e2c7c5451534
Bharathiraja2207 <bharathiraja2207@gmail.com>
2023-03-15 15:43:52 +0530

    comit

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,68 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import { Routes, Route, Link, useNavigate, useParams, Navigate } from "react-router-dom";
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import { Routes, Route, Link, useNavigate, useParams } from "react-router-dom";
 import InfoIcon from '@mui/icons-material/Info';
-
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import { Home } from './Home';
 
 export default function App() {
-  const [movieList, setMovieList] = useState([{
-    "_id": "63f8d87354972eaaeb617522",
-    "id": "102",
-    "name": "No Country for Old Men",
-    "poster": "https://upload.wikimedia.org/wikipedia/en/8/8b/No_Country_for_Old_Men_poster.jpg",
-    "rating": 8.1,
-    "summary": "A hunter's life takes a drastic turn when he discovers two million dollars while strolling through the aftermath of a drug deal. He is then pursued by a psychopathic killer who wants the money.",
-    "trailer": "https://www.youtube.com/embed/38A__WT3-o0"
-    },
-    {
-    "_id": "63f8d87354972eaaeb617529",
-    "name": "Thor: Ragnarok",
-    "poster": "https://m.media-amazon.com/images/M/MV5BMjMyNDkzMzI1OF5BMl5BanBnXkFtZTgwODcxODg5MjI@._V1_.jpg",
-    "summary": "When Earth becomes uninhabitable in the future, a farmer and ex-NASA\\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\\n of researchers, to find a new planet for humans.",
-    "rating": 8.8,
-    "trailer": "https://youtu.be/NgsQ8mVkN8w",
-    "id": "109"
-    },
-    {
-    "_id": "63f8d87354972eaaeb61751f",
-    "id": "99",
-    "name": "Vikram😍😍😍",
-    "poster": "https://m.media-amazon.com/images/M/MV5BMmJhYTYxMGEtNjQ5NS00MWZiLWEwN2ItYjJmMWE2YTU1YWYxXkEyXkFqcGdeQXVyMTEzNzg0Mjkx._V1_.jpg",
-    "rating": 8,
-    "summary": "Members of a black ops team must track and eliminate a gang of masked murderers.",
-    "trailer": "https://www.youtube.com/embed/OKBMCL-frPU"
-    }])
+
+  const [movieList, setMovieList] = useState([])
+  const navigate = useNavigate()
   return (
     <div className="App">
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static">
           <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 0.05 }}>
-              Home
-            </Typography>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Movies
-            </Typography>
-            <Button color="inherit">Login</Button>
+            <Button onClick={() => navigate("/")} color="inherit">Home</Button>
+            <Button onClick={() => navigate("/movies")} color="inherit">Movie</Button>
           </Toolbar>
         </AppBar>
       </Box>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<MovieList movieList={movieList} />} />
-        <Route path="/movies/:id" element={<MovieDetails movieList={movieList} setMovieList={setMovieList} />} />
+        <Route path="/seats" element={<Proudctedroute><Seats /></Proudctedroute>} />
+        <Route path="/showtime" element={<Theater/>} />
+        <Route path="/movies" element={<MovieList movieList={movieList} setMovieList={setMovieList} />} />
+        <Route path="/movies/:id" element={<MovieDetails movieList={movieList} />} />
       </Routes>
-      {/* <Home />
-      <MovieList /> */}
+
     </div>
   )
 }
 
+function Proudctedroute({children}){
+  const token=localStorage.getItem('token');
+  // const token=false;
+  return(
+   token? <section>{children}</section>:<Navigate replace to="/"/>
+  //  token? <section>{children}</section>:<h1>unautharaied</h1>
+  )
+}
+
 function MovieDetails({ movieList }) {
+  const navigate = useNavigate()
   const { id } = useParams()
-  const movie = movieList[id]
+  const [movie, setMovie] = useState([])
+  useEffect(() => {
+    fetch(`https://641061f4864814e5b64fe12e.mockapi.io/movies/${id}`)
+      .then((data) => data.json())
+      .then((mvs) => setMovie(mvs))
+  }, [id])
   return (
     <div>
-       <iframe
+      <iframe
         width="100%"
         height="650"
         src={movie.trailer}
@@ -79,17 +71,23 @@ function MovieDetails({ movieList }) {
         allow="accelerometer; autoplay;clipboard-white"
         allowfullscreen
       ></iframe>
-      Movie details {movie.name}</div>
+      <div className="movie-detail-container">
+        <h1>Movie Details of {movie.name}</h1>
+       
+            <p className="movie-summary">{movie.summary}</p>
+         
+        <Button onClick={() => navigate("/showtime")} color="inherit">book ticket</Button>
+      </div>
+    </div>
   )
 }
-function Home() {
-  return (
-    <div>Welcome to bookmyshow</div>
 
-  )
-}
-
-function MovieList({ movieList }) {
+function MovieList({ movieList, setMovieList }) {
+  useEffect(() => {
+    fetch("https://641061f4864814e5b64fe12e.mockapi.io/movies")
+      .then((data) => data.json())
+      .then((mvs) => setMovieList(mvs))
+  }, [])
 
   return (
     <div className="movie-list">
@@ -97,27 +95,83 @@ function MovieList({ movieList }) {
     </div>
   )
 }
+
+
 function Movies({ movie, id }) {
   const navigate = useNavigate()
   return (
-    <div className="movie-container">
-      <img className="movie-poster" src={movie.poster} alt={movie.name} />
+    <Card  onClick={() => navigate(`/movies/${id}`)}>
 
-      <div>
-        <div className="movie-data">
+      <div className="movie-container">
 
-          <p className="movie-name"><h2>{movie.name}</h2>
-          </p>
-          <IconButton color="primary" fontSize="small"
-            onClick={() => navigate(`/movies/${id}`)}
-            arial-label="movie-details">
-            <InfoIcon />
-          </IconButton>
-          <p className="movie-rating">⭐{movie.rating}</p>
+        <img className="movie-poster" src={movie.poster} alt={movie.name} />
+        <CardContent>
+          <div>
+            <div className="movie-data">
+              <CardActions>
+                <p className="movie-name"><h2>{movie.name}
+                  <IconButton color="primary" fontSize="small"
+                    onClick={() => navigate(`/movies/${id}`)}>
+                    <InfoIcon /></IconButton>
+                </h2></p>
+                <p className="movie-rating">⭐{movie.rating}</p>
+              </CardActions>
+            </div>
+            {/* <p className="movie-summary">{movie.summary}</p> */}
+          </div>
 
-        </div>
-        <p className="movie-summary">{movie.summary}</p>
+        </CardContent>
       </div >
-    </div >
+
+    </Card >
   )
+}
+
+function Theater(){
+  const showtime=[{"id":"1","theatername":"pss multiplex","show1":"09:00 am","show2":"09:00 pm"},
+  {"id":"2","theatername":"bharathi multiplex","show1":"09:00 am","show2":"09:00 pm"},
+  {"id":"3","theatername":"bhagavath multiplex","show1":"09:00 am","show2":"09:00 pm"},
+  {"id":"4","theatername":"fathima multiplex","show1":"09:00 am","show2":"09:00 pm"}]
+  return(
+    <div className='show'>
+      {showtime.map((mv) => <Showtime key={mv.id} show={mv} id={mv.id} />)}
+      </div>
+  )
+}
+
+
+function Showtime({show}){
+  const navigate = useNavigate()
+  return(
+    <div>
+      <h2>{show.theatername}</h2>
+      <Button onClick={() => navigate("/seats")} color="inherit" variant='contained'>{show.show1}</Button>
+      <Button onClick={() => navigate("/seats")} color="inherit" variant='contained'>{show.show2}</Button>
+      </div>
+  )
+}
+
+function Seats(){
+  const button = Array(100).fill(<CheckBoxOutlineBlankIcon />)
+    return (
+        <div className="seatlist">
+            {button.map((btn, i) => <Seatbooking key={i} button={btn} />)}
+        </div>
+    )
+
+}
+
+function Seatbooking({button,key}){
+  const [active, setActive] = useState(true)
+    const [count, setCount] = useState(0)
+    const handleClick = () => {
+        setCount(count + 1)
+        setActive(!active)
+        console.log("clicked")
+    }
+    return (
+        <div className="seats">
+            <Button color={active ? "success" : "inherit"} onClick={handleClick}>{button}{key}</Button>
+        </div>
+    )
 }
---
+++ src/Home.jsx
@@ -0,0 +1,130 @@
+import { useFormik } from "formik";
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+export function Home() {
+  return (
+    <div>Book my show<Login/><Signin/></div>
+    
+  );
+}
+
+
+function Login() {
+  const navigate=useNavigate()
+const [formstate,setformstate]=useState("success")
+
+  const formik = useFormik({
+    initialValues: {
+      username:'bharathi',
+      password:'password@123'
+    },
+    // validationSchema: formValidationSchema,
+    onSubmit: async (values) => {
+      // console.log(values)
+
+  const data=  await fetch("http://localhost:4000/users/login", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json"
+        },
+        body: JSON.stringify(values)
+      });
+      if(data.status===400){
+console.log("error");
+setformstate("error")
+      }else{
+        setformstate("success")
+        const result= await data.json()
+        console.log("success",result);
+        localStorage.setItem("token",result.token)
+        navigate("/seats")
+      }
+      
+    }
+  });
+  return (
+    <form onSubmit={formik.handleSubmit} className='loninform'>
+       <h2>login</h2>
+       <div className='loninfield'>
+       <TextField
+       name='username'
+        value={formik.values.username}
+        onChange={formik.handleChange}
+        label="username"
+        variant="outlined" />
+       <TextField 
+        value={formik.values.password}
+        onChange={formik.handleChange}
+        label="password" 
+        name="password" 
+        variant="outlined" />
+       <Button color={formstate} type='submit' variant="contained">{formstate==="success"?"submit":"retry"}</Button>
+       </div>
+
+       </form>
+    
+    
+  );
+}
+
+
+function Signin() {
+  const navigate=useNavigate()
+const [formstate,setformstate]=useState("success")
+
+  const formik = useFormik({
+    initialValues: {
+      username:'',
+      password:''
+    },
+    // validationSchema: formValidationSchema,
+    onSubmit: (newdata) => {
+      // console.log(values)
+      adddata(newdata)
+    }
+  });
+
+  const adddata= (newdata)=>{
+    console.log(newdata)
+
+       fetch("http://localhost:4000/users/signup",{
+        method:"POST",
+      body:JSON.stringify(newdata),
+    headers:{
+      "content-type":"application/json"
+    }
+       })
+       navigate("/")
+     };
+  return (
+    <form onSubmit={formik.handleSubmit} className='loninform'>
+       <h2>signup</h2>
+       <div className='loninfield'>
+       <TextField
+       placeholder="username"
+       name='username'
+        value={formik.values.username}
+        onBlur={formik.handleBlur}
+        onChange={formik.handleChange}
+        label="username"
+        variant="outlined" />
+       <TextField 
+       placeholder="password"
+        value={formik.values.password}
+        onBlur={formik.handleBlur}
+        onChange={formik.handleChange}
+        label="password" 
+        name="password" 
+        variant="outlined" />
+       <Button color="success" type='submit' variant="contained">submit</Button>
+       </div>
+
+       </form>
+    
+    
+  );
+}
+
